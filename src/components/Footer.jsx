029c94c52e25f0b9f2c1e936a2e2df0ef0065bf1
Youssef Jnah <youssefj86@gmail.com>
2024-09-13 13:30:37 +0200

    Résolution du problème du logo qui ne s'affichait pas après l'actualisation de la page && Retouches sur les styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import footerLogo from '/Logo/Logo-footer.svg'
 
 function Footer() {
   return (
     <div className='footer'>
-        <img src="Logo-footer.svg" alt="Logo Kasa" />
+        <img src={footerLogo}  alt="Logo Kasa" />
         <h3>© 2020 Kasa. All rights reserved</h3>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import logo from '/Logo/Logo.png'
 
 function Header() {
   return (
     <div className="header">
-      <img src="Logo.png" alt="Logo Kasa" />
+      <img src={logo} alt="Logo Kasa" />
       <nav>
         <ul>
             <NavLink to="/"><li>Accueil</li></NavLink>
